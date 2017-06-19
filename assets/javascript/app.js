15bbada02b73159351d37bba55a515317579872a
MB DR <mderoberts@users.noreply.github.com>
2017-06-18 23:38:01 -0400

    added questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+var questions = [{
+    question: "What item of the Dude's really tied the room together?",
+    choices: ["Photo of Richard Nixon bowling", "Recliner", "Rug", "Liquor cabinet"],
+    correctAnswer: 3
+}, {
+    question: "What was Walter concerned that nobody cares about?",
+    choices: ["Shabbos", "The rules", "Pomeranians", "Nihillists"],
+    correctAnswer: 2
+}, {
+    question: "What is the Dude's favorite band?",
+    choices: ["Creedance Clearwater Revival", "The Eagles", "Autobahn", "Bob Dylan"],
+    correctAnswer: 1
+}, {
+    question: "To whom does Bunny Lebowski owe money?",
+    choices: ["Pacifists", "Donny", "Larry", "Jackie Treehorn"],
+    correctAnswer: 4
+}, {
+    question: "Which fast-food restaurant did the Dude, Walter and Donny visit?",
+    choices: ["In-N-Out Burger", "Ralph's", "Wendy's", "McDonald's"],
+    correctAnswer: 1
+}];
