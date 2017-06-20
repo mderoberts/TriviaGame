b8b8a9cd8576f3e7c08a0d698728dbcd063519b7
MB DR <mderoberts@users.noreply.github.com>
2017-06-20 12:57:48 -0400

    js timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,6 @@
+$(document).ready(function () {
+
+// Questions
 var questions = [{
     question: "What item of the Dude's really tied the room together?",
     choices: ["Photo of Richard Nixon bowling", "Recliner", "Rug", "Liquor cabinet"],
@@ -19,3 +22,53 @@ var questions = [{
     choices: ["In-N-Out Burger", "Ralph's", "Wendy's", "McDonald's"],
     correctAnswer: 1
 }];
+
+// Timer
+var intervalID;
+var clockRunning = false;
+var timer = {
+    time: 0,
+    reset: function() {
+        timer.time = 0;
+    }
+    start: function() {
+        if (!clockRunning) {
+            intervalID = setInterval(timer.count, 15000);
+            clockRunning = true;
+        },
+    stop: function() {
+        clearInterval(intervalID);
+        clockRunning = false;
+    },
+    count: function() {
+        timer.time--;
+    }
+    }
+};
+
+// Question screen
+ 
+currentQuestion
+options
+answerMessage
+correctResponse
+answerImage
+
+var scoreboard = {
+    correctAnswers: 0,
+    incorrectAnswers: 0,
+    unanswered: 0
+}
+
+// When the game starts, get the first question and choices and load them into the DOM. Start the timer.
+function start = {
+}
+
+function 
+
+function reset = {
+
+}
+
+
+});
